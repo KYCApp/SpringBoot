2e75bdefcc08e42cdb2bfceacdd0d7e105c7799a
mohamed-moslemani <moslemanomohamed@gmail.com>
2025-09-16 23:12:49 +0300

    JWT authentication for signup login

---
+++ src/main/java/com/mohamedMoslemani/kyc/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.mohamedMoslemani.kyc.security;
+
+import com.mohamedMoslemani.kyc.model.User;
+import com.mohamedMoslemani.kyc.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepo;
+
+    public CustomUserDetailsService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword()) 
+                .roles(user.getRole().name()) 
+                .build();
+    }
+}
